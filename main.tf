8be6eb9f4956028946f17420987d606091112ab9
gagan11079 <131979375+gagan11079@users.noreply.github.com>
2024-10-20 18:52:49 +0530

    Update main.tf
    this is "my  second commit "

--- main.tf
+++ main.tf
@@ -1,3 +1,26 @@
-provdiers "aws"{
-access_keyid = ""
-secret_key
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+    access_key="AKIAZZYUHNE4ABE5IAEL"
+    secret_key="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"
+    region= "ap-south-1"
+
+}
+
+resource "aws_instance" "my_instance"{
+    ami = "ami-04a37924ffe27da53"
+    instance_type="t2.micro"
+  
+tags= {
+     name=" this is my first instance"
+
+  }
+}
+ubuntu@ubuntu-VMware-Virtual-Platform:~/Terraform/instance_cre$ 
